ac1f7b9d81ba7d0f8756d818bc54846224809f22
jonycmtt <jonyislamcmt@gmail.com>
2024-08-15 23:00:28 +0600

    [added] added images

--- src/pages/langding/AdvaiseBanner.tsx
+++ src/pages/langding/AdvaiseBanner.tsx
@@ -1,16 +1,17 @@
 const AdviseBanner = () => {
   return (
     <div
+      className="py-20 h-[400px] mb-20"
       style={{
         backgroundImage:
-          "url(https://gramotech.net/html/balad/images/deprtinfobg.jpg)",
+          "url(https://github.com/jonycmtt/ict-division-govt/blob/main/src/assets/img/dangu.jpg?raw=true)",
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
     >
       <div className="max-w-5xl mx-auto">
-        <h2>To Prevent Dengue</h2>
+        <h2 className="text-4xl font-bold text-white">To Prevent Dengue</h2>
         <button>Learn More</button>
       </div>
     </div>
